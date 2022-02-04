e7b24d82dd8fe231003fa9ad811d5dc3ed3224c4
valeriayunga <vayunga@utpl.edu.ec>
2022-02-03 20:13:24 -0500

    primer commit

--- codigo/run.py
+++ codigo/run.py
@@ -5,18 +5,55 @@
     Problemática:
 """
 
-def crearFacebook():
-    nombre=str(input(f"Creando Facebook\nIngrese el nombre del usuario: "))
-    edad=str(input("Ingrese la edad del usuario: "))
-    ciudad=str(input("Ingrese la ciudad donde reside el usuario: "))
-    pais=str(input("Ingrse el pais donde reside el usuario: "))
-    correo=str(input("ingrese el correo electrónico del usuario: "))
 
-    cadena=str("")
-    cadena=(f"{cadena}Resumen de cuenta creada:\nCreando Facebook\nUsuario: {nombre}\nEdad: {edad}\n"
-            f"Ciudad de residencia:{ciudad}"f"\nPais de residencia: {pais}\n"
-            f"Correo electrónico: {correo}")
+def main():
+    bandera = True
+    contador = 0
+    while bandera:
+        num = int(input("Ingrese el 1 para crear cuenta de Facebook\n"
+                        "Ingrese el 2 para crear cuenta de twitter\n"
+                        "Ingrese el 3 para crear cuenta de Whatsapp\n"
+                        "Ingrese el 4 para crear cuenta de Telegram\n"
+                        "Ingrese el 5 para crear cuenta de Signal\n"
+                        "Ingrese el 6 para crear cuenta de Instagram\n"
+                        "Ingrese el 7 para crear cuenta de Flickr\n"
+                        "Ingrese 0 para dejar de crear cuentas\n"))
+        if num == 1:
+            print(crearFacebook())
+        elif num == 2:
+            crearTwitter()
+        elif num == 3:
+            print(crearWhatsapp())
+        elif num == 4:
+            crearTelegram()
+        elif num == 5:
+            print(crearSignal())
+        elif num == 6:
+            crearInstagram()
+        elif num == 7:
+            print(crearFlickr())
+        elif (num == 0 or num < 0 or num > 7):
+            bandera = False
+            print("Proceso de creacion de cuentas finalizado")
+        if 1 <= num <= 7:
+            contador = contador + 1
+
+    cadena = "\nNumero de cuentas creadas: %d --- %s\n" % (contador, obtenerMensaje(contador))
+    print(cadena)
+
+
+def crearFacebook():
+    nombre = str(input(f"Creando Facebook\nIngrese el nombre del usuario: "))
+    edad = str(input("Ingrese la edad del usuario: "))
+    ciudad = str(input("Ingrese la ciudad donde reside el usuario: "))
+    pais = str(input("Ingrse el pais donde reside el usuario: "))
+    correo = str(input("Ingrese el correo electrónico del usuario: "))
+
+    cadena = str("")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nEdad: %s\nCiudad de residencia:%s\n"
+              "Pais de residencia: %s\nCorreo electrónico: %s\n" % (cadena, nombre, edad, ciudad, pais, correo))
     return cadena
+
 
 def crearTwitter():
     nombreU = str(input(f"Creando Twitter\nIngrese el nombre del usuario: "))
@@ -29,22 +66,24 @@ def crearTwitter():
     correo = str(input("Ingrese el correo electrónico del usuario: "))
 
     cadena = str("")
-    cadena = (f"{cadena}Resumen de cuenta creada:\nUsuario: {nombreU}\nNombres:{nombres}\nApellidos: {apellidos}"
-              f" Edad: {edad}\nCiudad de residencia:{ciudad}\nPais de residencia: {pais}\nIdioma: {idioma}"
-              f"Correo electrónico: {correo}\n")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nNombres: %s\nApellidos: %s"
+              " Edad: %s\nCiudad de residencia:%s\nPais de residencia: %s\nIdioma: %s"
+              "Correo electrónico: %s\n" % (cadena, nombreU, nombres, apellidos, edad, ciudad, pais, idioma, correo))
     print(cadena)
 
-def crearWhatsapp():
-    nombre=str(input(f"Creando Whatsapp\nIngrese el nombre del usuario: "))
-    numeroTelefono=str(input("Ingrese su numero de telefono: "))
-    edad=str(input("Ingrese la edad del usuario: "))
-    ciudad=str(input("Ingrese la ciudad donde reside el usuario: "))
-    pais=str(input("Ingrese el pais donde reside el usuario: "))
 
-    cadena=str("")
-    cadena=(f"{cadena}Resumen de cuenta creada:\nUsuario: {nombre}\nNumero de Telefono: {numeroTelefono}\n Edad: "
-            f"{edad}\nCiudad de residencia:{ciudad}\nPais de residencia: {pais}\n")
+def crearWhatsapp():
+    nombre = str(input(f"Creando Whatsapp\nIngrese el nombre del usuario: "))
+    numero = str(input("Ingrese su numero de telefono: "))
+    edad = str(input("Ingrese la edad del usuario: "))
+    ciudad = str(input("Ingrese la ciudad donde reside el usuario: "))
+    pais = str(input("Ingrese el pais donde reside el usuario: "))
+
+    cadena = str("")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nNumero de Telefono: %s\n Edad: "
+              "%s\nCiudad de residencia:%s\nPais de residencia: %s\n" % (cadena, nombre, numero, edad, ciudad, pais))
     return cadena
+
 
 def crearTelegram():
     nombreU = str(input(f"Creando Telegram\nIngrese el nombre del usuario: "))
@@ -53,83 +92,56 @@ def crearTelegram():
     pais = str(input("Ingrese el pais donde reside el usuario: "))
     hobby = str(input("Ingrese su area de interés: "))
 
-
     cadena = str("")
-    cadena = (f"{cadena}Resumen de cuenta creada:\nUsuario: {nombreU}\nNúmero de teléfono: {numero}\n"
-              f"Ciudad de residencia:{ciudad}\nPais de residencia: {pais}\nHobby favorito: {hobby}\n")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nNúmero de teléfono: %s\n"
+              "Ciudad de residencia:%s\nPais de residencia: %s\nHobby favorito: %s\n"
+              % (cadena, nombreU, numero, ciudad, pais, hobby))
     print(cadena)
 
-def crearSignal():
-    nombre=str(input(f"Creando Signal\nIngrese el nombre del usuario: "))
-    numeroTelefono=str(input("Ingrese su numero de telefono: "))
-    ciudad=str(input("Ingrese la ciudad donde reside el usuario: "))
-    pais=str(input("Ingrese el pais donde reside el usuario: "))
-    hobby=str(input("Ingrese su hobby preferido: "))
 
-    cadena=str("")
-    cadena=(f"{cadena}Resumen de cuenta creada: \nUsuario: {nombre}\nNumero de Telefono: {numeroTelefono}\n"
-            f"Ciudad de residencia:{ciudad}\nPais de residencia: {pais}\nHobby Principal: {hobby}\n")
+def crearSignal():
+    nombre = str(input(f"Creando Signal\nIngrese el nombre del usuario: "))
+    numero = str(input("Ingrese su numero de telefono: "))
+    ciudad = str(input("Ingrese la ciudad donde reside el usuario: "))
+    pais = str(input("Ingrese el pais donde reside el usuario: "))
+    hobby = str(input("Ingrese su hobby preferido: "))
+
+    cadena = str("")
+    cadena = ("%sResumen de cuenta creada: \nUsuario: %s\nNumero de Telefono: %s\n"
+              "Ciudad de residencia:%s\nPais de residencia: %s\nHobby Principal: %s\n"
+              % (cadena, nombre, numero, ciudad, pais, hobby))
     return cadena
+
 
 def crearInstagram():
     nombreU = str(input(f"Creando Instagram\nIngrese el nombre del usuario: "))
     ciudad = str(input("Ingrese la ciudad donde reside el usuario: "))
-    edad = int(input("Ingrese la edad del usuario: "))
+    edad = str(input("Ingrese la edad del usuario: "))
     correo = str(input("ingrese el correo electrónico del usuario: "))
 
     cadena = str("")
-    cadena = (f"{cadena}Resumen de cuenta creada:\nUsuario: {nombreU}\nCiudad de residencia"
-              f":{ciudad}\nEdad: {edad}\nCorreo electrónico: {correo}\n")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nCiudad de residencia"
+              ":%s\nEdad: %s\nCorreo electrónico: %s\n" % (cadena, nombreU, ciudad, edad, correo))
     print(cadena)
 
-def crearFlickr():
-    nombre=str(input(f"Creando Flickr\nIngrese el nombre del usuario: "))
-    correo=str(input("Ingrese su correo: "))
 
-    cadena=str("")
-    cadena=(f"{cadena}Resumen de cuenta creada:\nUsuario: {nombre}\nCorreo Electrónico: {correo}\n")
+def crearFlickr():
+    nombre = str(input(f"Creando Flickr\nIngrese el nombre del usuario: "))
+    correo = str(input("Ingrese su correo: "))
+
+    cadena = str("")
+    cadena = ("%sResumen de cuenta creada:\nUsuario: %s\nCorreo Electrónico: %s\n" % (cadena, nombre, correo))
     return cadena
 
-if __name__ == "__main__":
-    bandera= True
-    contador= 0
+
+def obtenerMensaje(a):
     mensajeFinal = ["Campaña con poca afluencia", "Campaña moderada siga adelante", "Excelente campaña"]
-    while (bandera):
 
-        eleccion = int(input(f"Ingrese 1 si desea crear un cuenta de Facebook\n"
-                             f"Ingrese 2 si desea crear un cuenta de Twitter\n"
-                             f"Ingrese 3 si desea crear un cuenta de Whatsapp\n"
-                             f"Ingrese 4 si desea crear una cuentade Telegram\n"
-                             f"Ingrese 5 si desea crear una cuenta de Signal\n"
-                             f"Ingrese 6 si desea crear una cuenta de Instagram\n"
-                             f"Ingrese 7 si dese crear una cuenta de Flickr\n"
-                             f"Ingrese 0 para salir dejar de crear cuentas\n"))
+    if (a >= 1 and a <= 5):
+        return (mensajeFinal[0])
+    elif (a >= 6 and a <= 15):
+        return (mensajeFinal[1])
+    elif (a >= 16):
+        return (mensajeFinal[2])
 
-        if (eleccion == 1):
-            print(crearFacebook())
-        elif(eleccion == 2):
-            crearTwitter()
-        elif(eleccion == 3):
-            print(crearWhatsapp())
-        elif(eleccion == 4):
-            crearTelegram()
-        elif(eleccion == 5):
-            print(crearSignal())
-        elif(eleccion == 6):
-            crearInstagram()
-        elif(eleccion == 7):
-            print(crearFlickr())
-        elif(eleccion<0 or eleccion >7):
-            bandera = False
-            print("Valores introducidos fuera de rango")
-        elif(eleccion == 0):
-            bandera = False
-            print(f"Proceso de creaccion de cuentas finalizado\nNumero de cuentas creadas: {contador}")
-            if(contador >=1 and contador<=5):
-                print(mensajeFinal[0])
-            elif(contador >=6 and contador<=15):
-                print(mensajeFinal[1])
-            elif(contador >=16):
-                print(mensajeFinal[2])
-        contador = contador + 1
-
+print(main())
